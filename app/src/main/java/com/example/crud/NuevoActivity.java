f66bed5b6f69e2559f5d3f97242676b044f8c321
Stheven <103013359+Leoxip@users.noreply.github.com>
2023-09-04 19:50:23 -0500

    R

--- app/src/main/java/com/example/crud/NuevoActivity.java
+++ app/src/main/java/com/example/crud/NuevoActivity.java
@@ -3,12 +3,21 @@ package com.example.crud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NuevoActivity extends AppCompatActivity {
 
+    EditText txtNombre, txtTelefono, txtCorreoElectronico
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtNombre = findViewById(R.id.txtNombre);
+        
+
     }
 }
